5d4df26bdd1628f2b48fe8a511e708db87707efd
Maxwell Macedo <maxwellmacedo2015@gmail.com>
2025-05-05 02:53:08 -0300

    refactor: remove 'in' from bearer auth config

--- src/main.ts
+++ src/main.ts
@@ -26,9 +26,7 @@ async function bootstrap() {
         type: 'http',
         scheme: 'bearer',
         bearerFormat: 'JWT',
-        name: 'Authorization',
         description: 'Enter JWT token',
-        in: 'header',
       },
       'jwt',
     )
